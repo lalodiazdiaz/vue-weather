182b33de037327b75c11095b7a4adba6533b81b0
lalodiazdiaz <eduardodiazdiaz93@gmail.com>
2023-07-11 12:26:22 -0600

    image changed

--- src/App.vue
+++ src/App.vue
@@ -107,14 +107,14 @@ body {
 }
 
 #app {
-  background-image: url("./assets/cold-bg.jpg");
-  background-size: contain;
+  background-image: url("./assets/cold.jpg");
+  background-size: cover;
   background-position: bottom;
   transition: 0.4s;
 }
 
 #app.warm {
-  background-image: url("./assets//warm-bg.jpg");
+  background-image: url("./assets//warm.jpg");
 }
 
 main {
